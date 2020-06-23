a2cd021e3c796b120ac5e6c2baaa771f550567c4
jithums2792 <greenox2792@gmail.com>
2020-06-23 16:34:37 +0530

    exame result

--- src/app/student/exams/exams.component.ts
+++ src/app/student/exams/exams.component.ts
@@ -3,6 +3,7 @@ import { Router, NavigationExtras } from '@angular/router';
 import { StudentsService } from 'src/app/services/students.service';
 import { ExamService } from 'src/app/services/exam.service';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { AnswerService } from 'src/app/services/answer.service';
 
 @Component({
   selector: 'app-exams',
@@ -14,8 +15,14 @@ export class ExamsComponent implements OnInit {
     class: null,
     section: null
   }
+  public answerQuery = {
+    room: localStorage.getItem('studentclass'),
+    section: localStorage.getItem('studentsection'),
+    name: localStorage.getItem('studentname')
+  }
   public studentInfo
   public examList = []
+  public answerList = []
   public currentTime:string
   public modalRef: BsModalRef;
   public exam
@@ -24,14 +31,13 @@ export class ExamsComponent implements OnInit {
   constructor(private router: Router,
               private studentservice: StudentsService,
               private modalService: BsModalService,
+              private answerservice: AnswerService,
               private examservice: ExamService) { }
 
   ngOnInit() {
     this.getStudentinfo()
     this.getCurrentTime()
-    // setInterval(() => {
-    //    this.getCurrentTime()
-    // },1000)
+    this.getAnswerlist()
  }
 
   async getCurrentTime() {
@@ -51,6 +57,14 @@ export class ExamsComponent implements OnInit {
 
   async getExamList() {
     this.examservice.getExambyCategory(this.query).subscribe(data => this.examList = data.data)
+  }
+
+  async getAnswerlist() {
+    console.log(this.answerQuery)
+    this.answerservice.getanswerbyCategory(this.answerQuery).subscribe(data => {
+      this.answerList = data.data
+      console.log(this.answerList)
+    })
   }
 
   async navigate(exam, instruction: TemplateRef<any>) {
--- src/app/teachers/answerlist/answerlist.component.ts
+++ src/app/teachers/answerlist/answerlist.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef  } from '@angular/core';
 import { Router } from '@angular/router';
 import { AnswerService } from 'src/app/services/answer.service';
 import { ToastrService } from 'ngx-toastr';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-answerlist',
@@ -10,6 +11,7 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class AnswerlistComponent implements OnInit {
   public exam
+  public answerSheet
   public query = {
     examname: null,
     room: null,
@@ -17,8 +19,10 @@ export class AnswerlistComponent implements OnInit {
     subject: null,
     teacher: null
   }
+  public modalRef: BsModalRef;
   public answersheetList = []
   constructor(private router: Router,
+    private modalService: BsModalService,
               private answerservice: AnswerService,
               private toastservice: ToastrService) { 
     try {
@@ -46,6 +50,19 @@ export class AnswerlistComponent implements OnInit {
       } else {
         this.toastservice.warning('No answerSheets')
         this.router.navigate(['/teacher/home/exams'])
+      }
+    })
+  }
+
+  openModal(template: TemplateRef<any>, sheet) {
+    this.modalRef = this.modalService.show(template)
+    this.answerSheet = sheet
+  }
+
+  submit() {
+    this.answerservice.updateanswer(this.answerSheet._id, this.answerSheet).subscribe(data => {
+      if (data.status === 'success') {
+        this.toastservice.success('mark added', 'success')
       }
     })
   }
